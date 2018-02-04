dc250d6d52458afa9011c52c00e7abef95b08554
Pedro Pombeiro <pombeirp@users.noreply.github.com>
2018-02-04 19:09:13 +0100

    Implement rudimentary version of dynamic Slack to GitHub ID username mapping. Part of #12

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-module.exports = (robot) => {
+var MemCache = require('mem-cache')
+var SlackGitHubCacheBuilder = require('./lib/retrieve-slack-github-users')
+
+module.exports = async (robot) => {
   console.log('Yay, the app was loaded!')
+
+  var slackGitHubCache = new MemCache({ timeoutDisabled: true })
+  var slackCachePromise = SlackGitHubCacheBuilder.build(robot, slackGitHubCache)
 
   require('./scripts/assign-new-pr-to-review')(robot)
   require('./scripts/assign-approved-pr-to-test')(robot)
   require('./scripts/assign-to-bounty-awaiting-for-approval')(robot)
   require('./scripts/greet-new-contributor')(robot)
+
+  await slackCachePromise
+
+  // Add scripts which require using the Slack/GitHub cache after this comment
 
   // For more information on building apps:
   // https://probot.github.io/docs/
